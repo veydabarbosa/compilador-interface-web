e64429241751244d6082f1909d406a3f0a9c1d07
Vitor Wöstehoff <VitorWostehoffBNU@gmail.com>
2026-08-21 10:56:40 -0300

    Finalização da classe CompiladorController

--- src/main/java/controller/CompiladorController.java
+++ src/main/java/controller/CompiladorController.java
@@ -1,7 +1,5 @@
 package controller;
 
-//Vitor e Nicole
-
 import model.Arquivo;
 import service.ArquivoService;
 import view.AreaMensagens;
@@ -19,32 +17,44 @@ import java.io.IOException;
 import java.util.function.Consumer;
 
 public class CompiladorController {
+
     private final Editor editor;
     private final AreaMensagens areaMensagens;
     private final BarraStatus barraStatus;
     private final ArquivoService arquivoService;
+    private final Arquivo arquivo;
 
-    public CompiladorController (Editor editor, AreaMensagens areaMensagens, BarraStatus barraStatus, ArquivoService arquivoService){
+    public CompiladorController(Editor editor, AreaMensagens areaMensagens, BarraStatus barraStatus,
+                                ArquivoService arquivoService, Arquivo arquivo) {
         this.editor = editor;
         this.areaMensagens = areaMensagens;
         this.barraStatus = barraStatus;
         this.arquivoService = arquivoService;
+        this.arquivo = arquivo;
     }
 
-    public void registrar(BarraFerramentas barra, JRootPane rootPane){
-        barra.getBtnAbrir().addActionListener(this::abrir);
-        registrarAtalho(rootPane, "abrir", KeyEvent.VK_0, InputEvent.CTRL_DOWN_MASK, this::abrir);
+    public void registrar(BarraFerramentas barra, JRootPane rootPane) {
 
-        barra.getBtnCopiar().addActionListener(e -> editor.getTextArea().copy());
-        barra.getBtnColar().addActionListener(e -> editor.getTextArea().paste());
-        barra.getBtnRecortar().addActionListener(e -> editor.getTextArea().cut());
+        //ActionListener do botão NOVO
+
+        barra.getBtnAbrir().addActionListener(this::abrir);
+        registrarAtalho(rootPane, "abrir", KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK, this::abrir);
+
+        //ActionListener do botão SALVAR
+
+        barra.getBtnCopiar().addActionListener(e -> editor.getAreaTexto().copy());
+        barra.getBtnColar().addActionListener(e -> editor.getAreaTexto().paste());
+        barra.getBtnRecortar().addActionListener(e -> editor.getAreaTexto().cut());
+
         barra.getBtncompilar().addActionListener(this::compilar);
         registrarAtalho(rootPane, "compilar", KeyEvent.VK_F7, 0, this::compilar);
+
         barra.getBtnEquipe().addActionListener(this::equipe);
-        registrarAtalho(rootPane, "equipe", KeyEvent.VK_F1, 0 , this::equipe);
+        registrarAtalho(rootPane, "equipe", KeyEvent.VK_F1, 0, this::equipe);
     }
 
-    private void registrarAtalho(JRootPane rootPane, String nome, int tecla, int modificador, Consumer<ActionEvent> acao) {
+    private void registrarAtalho(JRootPane rootPane, String nome, int tecla, int modificador,
+                                 Consumer<ActionEvent> acao) {
         KeyStroke keyStroke = KeyStroke.getKeyStroke(tecla, modificador);
         rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(keyStroke, nome);
         rootPane.getActionMap().put(nome, new AbstractAction() {
@@ -55,22 +65,40 @@ public class CompiladorController {
         });
     }
 
-    /*
-    private void abrir (ActionEvent e){
+    //Função NOVO
+
+    private void abrir(ActionEvent e) {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setFileFilter(new FileNameExtensionFilter("Arquivo de texto (*.txt)", "txt"));
 
         int resultado = fileChooser.showOpenDialog(SwingUtilities.getWindowAncestor(editor));
 
-        if (resultado != JFileChooser.APPROVE_OPTION){
+        if (resultado != JFileChooser.APPROVE_OPTION) {
             return;
         }
 
         File arquivoSelecionado = fileChooser.getSelectedFile();
         try {
-            Arquivo arquivo = arquivoService.abrir(arquivoSelecionado);
-            editor.setConteudo(arquivo.getConteudo());
+            String conteudo = arquivoService.abrir(arquivoSelecionado.toPath());
+            editor.setTexto(conteudo);
+            areaMensagens.limpar();
+            arquivo.setCaminho(arquivoSelecionado.toPath());
+            barraStatus.mostrarArquivo(arquivo.getCaminho());
+        } catch (IOException ex) {
+            areaMensagens.limpar();
+            areaMensagens.mostrarMensagem("Erro ao abrir o arquivo: " + ex.getMessage());
         }
     }
-    */
+
+    //Função Salvar
+
+    private void compilar(ActionEvent e) {
+        areaMensagens.limpar();
+        areaMensagens.mostrarMensagem("Compilação de programas ainda não foi implementada.");
+    }
+
+    private void equipe(ActionEvent e) {
+        areaMensagens.limpar();
+        areaMensagens.mostrarMensagem("Equipe de desenvolvimento:\nNicole Bruch\nVeyda Cristina Barbosa\nVitor W.");
+    }
 }
